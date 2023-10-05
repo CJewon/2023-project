4a761ccde902e9af17507858842cb5f6db889f97
CJewon <112820744+CJewon@users.noreply.github.com>
2023-10-05 20:08:28 +0900

    asd

--- nene/js/index.js
+++
@@ -1,1 +0,0 @@
-const indexDots = document.querySelector("index-dots");
--- nene/js/menuSlideActive.js
+++ nene/js/menuSlideActive.js
@@ -1,7 +1,8 @@
 // 필요한 요소 : ChildElementCount를 이용해서 menu-slide-index-container의 자식의 갯수를 불러와서 index 값을 구한다.
+// menu-slide-index-container , 
 // 각 index값의 거리 값을 구하기 위한 식은
 // for(let i = 0; i < index; i ++) {
-//      i / 100 * index
+//      i / (index - 1) * 100
 // }
 //   결과값이 0 , 50%, 100% 가 나오게 해야한다
 //
@@ -12,3 +13,28 @@
 // index = 2 일떄 100%
 
 // index값이 바뀔때마다 결과값이 바뀌게 된다.
+
+// inedxArray[0]에서 첫번째 자식에게 left : 0 , 두번째 자식에게 left : 50% , 세번째 자식에게 left : 100% 를 줘야한다
+const indexContainer = document.getElementsByClassName('menu-slide-index-container'); 
+const indexDots = document.getElementsByClassName('index-dots'); // 10
+const indexArray = [];
+const indexElement = indexDots.length;
+
+for (let i = 0; i < indexContainer.length; i++) {
+    const childCount = indexContainer[i].childElementCount;
+    indexArray.push(childCount);
+  }
+
+
+
+
+
+for(let i = 0; i < indexArray.length; i++) {
+    for(let j = 0 ; j < indexArray[0]; j++) {
+        indexDots.style.left = (j / (indexArray[0] - 1) * 100);
+    } 
+}
+
+
+
+
