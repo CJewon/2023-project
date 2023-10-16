553f8c92a5830b06b963a386011ce2a6c480ce79
CJewon <112820744+CJewon@users.noreply.github.com>
2023-10-16 17:38:57 +0900

    awefasdf

--- nene/js/header.js
+++ nene/js/header.js
@@ -1,3 +1,27 @@
+// footer 컨텐츠가 화면에 나올때(footer.getboundingClientRect().top === 0) header를 display: none 처리해준다
+// footer 컨텐츠가 화면에 보이지 않을때(footer.getboundingClientRect().bottom === 0) header를 원래대로 되돌아온다.
+// scroll했을때 footer.getBoundingRect().top , bottom 값을 갱신해주고 갱신한 값으로 작동하게 해야한다.
+// 이를 위한 방법은 class를 add, remove 해주는 방법을 사용하면 될것이다.
+
+
+
+
+//  header.js에서는 header의 애니메이션만 만들어야한다.
 const header = document.querySelector('header');
 
-const headerValue = window.innerHeight;
+
+
+
+
+
+// 밑에 있는 코드는 header.js에 작성이 아닌 onepage.js에 추가해야하는 코드
+const footer = document.querySelector('footer');
+
+if(footer.getBoundingClientRect().top - innerHeight < 0) {
+ 
+    header.classList.add('active');
+}else if(footer.getBoundingClientRect().bottom < 0) {
+    header.classList.remove('active');
+}
+
+console.log(footer);
--- nene/js/onepage.js
+++ nene/js/onepage.js
@@ -1,22 +1,23 @@
+//  1. scroll이 실행될때마다 애니메이션이 실행이 되어야한다.(가속도, 애니메이션이 실행되는 시간)
+//  2. 스크롤 이벤트리스너가 실행이 되고 있으면 실행되는동안에는 스크롤 이벤트리스너가 실행이 되어서는 안된다.
+//  3. footer.getBoundingRect().top - innerHeight값이 0보다 작거나 같을때? window.scrollY 값이 innerHeight가 아닌 footer.clientHeight 만큼 증감소하게 해준다.
+//  4. footer.getBoundingRect(). - innerHeight값이 0일때 window.scrollY 값이 innerHeight가 아닌 footer.clientHeight 만큼 증감소하게 해준다.
+//  5. 만약 위와 같은 방법으로 코딩을 한다면 footer.getBoundingRect().top - innerHeight === 0 일때 두개의 이벤트 리스너가 겹칠수도 있다. 이에 대한 해결방법은 ????
+//  
+//
+//
+//
+//
+//
+//  마지막 footer은 길이가 다르므로 footer 길이만큼 한번의 스크롤을 통해 footer를 다 보여주게하고, header가 사라지게 만든다.
+
 window.addEventListener('mousewheel', (e) => {
-
-    if (e.wheelDelta > 0) {
-        if (window.scrollY === 0 ) {
-            e.preventDefault();
-        }
-    } else {
-        //마우스휠 아래로
-
-        if (window.scrollY === 0 ) {
-            e.preventDefault();
-
-        }
+    if (e.deltaY > 0) {
+        // 아래로 스크롤하는 경우
+        window.scrollBy(0, innerHeight);
+    } else if (e.deltaY < 0) {
+        // 위로 스크롤하는 경우
+        window.scrollBy(0, -innerHeight);
     }
-    console.log(e);
-    console.log(innerHeight);
 
-
-
-
-
-}, { passive: false })
+})
