efbd1ad0b38753af75064a71d0b42c1ec4596b34
DDogn929 <112820744+CJewon@users.noreply.github.com>
2023-10-20 13:09:19 +0900

    ads

--- nene/js/onepage.js
+++ nene/js/onepage.js
@@ -22,36 +22,32 @@ let moveState = false;
 window.addEventListener('mousewheel', (e) => {
     // addEventListener가 실행이 될때, moveState가 true가 되고, true가 되면서 스크롤이 시작이 된다. 근데 이것을 setTimeOut으로 통제를 한다.
     
-    if(!moveState) {
-        
-        setTimeout(() => {
-            
-        }, 0.5);
-    }
 
-    // console.log(sections.length);
-    if(window.scrollY < (sections.length)*window.innerHeight){   
-        e.preventDefault();
+    
         
-        if (e.deltaY > 0) {
-            index++;
-            // 아래로 스크롤하는 경우
-            window.scrollTo({top:index*window.innerHeight,left:0,behavior:"smooth"});
-            if(index > sections.length) {
-                index = sections.length;
-            }
+        // console.log(sections.length);
+        if(window.scrollY < (sections.length)*window.innerHeight){   
+            e.preventDefault();
             
-        } else if (e.deltaY < 0) {
-            // 위로 스크롤하는 경우
-            index--;
-            window.scrollTo({top:index*window.innerHeight,left:0,behavior:"smooth"});
-            if (index < 0) {
-                index = 0;
+            if (e.deltaY > 0) {
+                index++;
+                // 아래로 스크롤하는 경우
+                window.scrollTo({top:index*window.innerHeight,left:0,behavior:"smooth"});
+                if(index > sections.length) {
+                    index = sections.length;
+                }
+                
+            } else if (e.deltaY < 0) {
+                // 위로 스크롤하는 경우
+                index--;
+                window.scrollTo({top:index*window.innerHeight,left:0,behavior:"smooth"});
+                if (index < 0) {
+                    index = 0;
+                }
             }
-        }
-    } else if (window.scrollY > (sections.length)*window.innerHeight) {
-
-       
+        } else if (window.scrollY > (sections.length)*window.innerHeight) {
+            
+            
             if (e.deltaY > 0) {
                 // 아래로 스크롤하는 경우
                 window.scrollTo({top: index*window.innerHeight + footer.clientHeight,left:0,behavior:"smooth"});
@@ -61,30 +57,33 @@ window.addEventListener('mousewheel', (e) => {
                 window.scrollTo({top: index*window.innerHeight,left:0,behavior:"smooth"});
                 header.classList.remove('active');
             }
-        
-    } else if (window.scrollY = (sections.length)*window.innerHeight) {
-        if (e.deltaY > 0) {
-            // 아래로 스크롤하는 경우
-            window.scrollTo({top: index*window.innerHeight + footer.clientHeight,left:0,behavior:"smooth"});
-            header.classList.add('active');
             
-        } else if (e.deltaY < 0) {
-            // 위로 스크롤하는 경우
-            index--;
-            window.scrollTo({top: index*window.innerHeight,left:0,behavior:"smooth"});
-            
+        } else if (window.scrollY = (sections.length)*window.innerHeight) {
+            if (e.deltaY > 0) {
+                // 아래로 스크롤하는 경우
+                window.scrollTo({top: index*window.innerHeight + footer.clientHeight,left:0,behavior:"smooth"});
+                header.classList.add('active');
+                
+            } else if (e.deltaY < 0) {
+                // 위로 스크롤하는 경우
+                index--;
+                window.scrollTo({top: index*window.innerHeight,left:0,behavior:"smooth"});
+                
+            }
         }
-    }
+        
+        
+        
+        
+        // console.log('indexText : ' + Math.floor(window.scrollY / window.innerHeight))
+        // console.log(window.scrollY)
+        // console.log((sections.length-1)*window.innerHeight)
+        console.log(index);
+        
 
-   
-
-
-    // console.log('indexText : ' + Math.floor(window.scrollY / window.innerHeight))
-    // console.log(window.scrollY)
-    // console.log((sections.length-1)*window.innerHeight)
-    console.log(index);
-
-},{passive: false})
-
-// window.scrollY = (sections.length-1) * window.innerHeight 인 상황에서, e.deltaY > 0 일때 footer.clinetHeight 만큼 내려가고, e.deltaY < 0 일때 index --; 를하고 index*window.innerHeight 만큼 올라가게 한다.
-// index의 범위는 음수보다 크고 sections.length까지 올라오게 만든다.
+        
+        
+    },{passive: false})
+    
+    // window.scrollY = (sections.length-1) * window.innerHeight 인 상황에서, e.deltaY > 0 일때 footer.clinetHeight 만큼 내려가고, e.deltaY < 0 일때 index --; 를하고 index*window.innerHeight 만큼 올라가게 한다.
+    // index의 범위는 음수보다 크고 sections.length까지 올라오게 만든다.
