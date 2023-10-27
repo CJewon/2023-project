f4b5f9ed10a9576d5486b30c8c74c6093285c553
CJewon <112820744+CJewon@users.noreply.github.com>
2023-10-27 16:56:17 +0900

    asdf

--- nene/js/onepage.js
+++ nene/js/onepage.js
@@ -16,6 +16,7 @@ const header = document.querySelector('header');
 const footer = document.querySelector('footer');
 let index=0; // 현재페이지
 let moveState = false;
+let num = true;
 // console.log(header);
 
 
@@ -28,13 +29,10 @@ window.addEventListener('mousewheel', (e) => {
         setTimeout(() => {
             moveState=false;
         }, 500);
-        
-        
-        
+
         // console.log(sections.length);
         if(window.scrollY < (sections.length)*window.innerHeight){   
             e.preventDefault();
-            
             if (e.deltaY > 0) {
                 index++;
                 // 아래로 스크롤하는 경우
@@ -50,50 +48,37 @@ window.addEventListener('mousewheel', (e) => {
                 if (index < 0) {
                     index = 0;
                 }
-            }
-        } else if (window.scrollY > (sections.length)*window.innerHeight) {
-            
-            
-            if (e.deltaY > 0) {
-                // 아래로 스크롤하는 경우
-                window.scrollTo({top: index*window.innerHeight + footer.clientHeight,left:0,behavior:"smooth"});
-                
-            } else if (e.deltaY < 0) {
-                // 위로 스크롤하는 경우
-                window.scrollTo({top: index*window.innerHeight,left:0,behavior:"smooth"});
-                header.classList.remove('active');
-            }
-            
+                console.log(index);
+            }            
         } else if (window.scrollY = (sections.length)*window.innerHeight) {
             if (e.deltaY > 0) {
                 // 아래로 스크롤하는 경우
                 window.scrollTo({top: index*window.innerHeight + footer.clientHeight,left:0,behavior:"smooth"});
-                header.classList.add('active');
+                // header.classList.add('active');
+                console.log(header);
                 
             } else if (e.deltaY < 0) {
                 // 위로 스크롤하는 경우
                 index--;
                 window.scrollTo({top: index*window.innerHeight,left:0,behavior:"smooth"});
-                
+                console.log(header);
             }
         }
+        headerState();
         }
         
         
-        
-        
-        // console.log('indexText : ' + Math.floor(window.scrollY / window.innerHeight))
-        // console.log(window.scrollY)
-        // console.log((sections.length-1)*window.innerHeight)
-        
-        
-        
-        // console.log(index);
-        
-
         
         
     },{passive: false})
     
     // window.scrollY = (sections.length-1) * window.innerHeight 인 상황에서, e.deltaY > 0 일때 footer.clinetHeight 만큼 내려가고, e.deltaY < 0 일때 index --; 를하고 index*window.innerHeight 만큼 올라가게 한다.
     // index의 범위는 음수보다 크고 sections.length까지 올라오게 만든다.
+
+    function headerState() {
+        if (index > sections.length - 1) {
+            header.style.transform = `translateY(-${100}%)`;
+        } else {
+            header.style.transform = `translateY(${0}%)`;
+        }
+    }
