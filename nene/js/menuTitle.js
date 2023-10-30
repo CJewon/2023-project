b153d13122752bad48e2e785a14f9122164f6f81
CJewon <112820744+CJewon@users.noreply.github.com>
2023-10-30 21:13:12 +0900

    awef

--- nene/js/menuTitle.js
+++ nene/js/menuTitle.js
@@ -1,12 +1,12 @@
 // json 사용할때 기본형태 !! *** 데이터베이스 불러오기
 
-let 데이터베이스;
-fetch('../json/new.json')
-.then(res => res.json())
-.then(data => {
-  데이터베이스 = data
-  console.log(data[0]);
-})
+// let 데이터베이스;
+// fetch('../json/new.json')
+// .then(res => res.json())
+// .then(data => {
+//   데이터베이스 = data
+//   console.log(data[0]);
+// })
 
 // ************************************
 
@@ -15,17 +15,17 @@ fetch('../json/new.json')
 for(let i = 0; i < sectionSlider.length; i++) {
   const menuContainer = sectionSlider[i].firstElementChild;
   const menuContainerUl = menuContainer.firstElementChild;
-  const menuContainerLi = menuContainerUl.children;
+  const menuContainerLi = menuContainerUl
   
-  for(let j = 0 ; j <menuContainerLi.firstElementChild.length; j++ ){
+  // for(let j = 0 ; j <menuContainerLi.firstElementChild; j++ ){
 
-    // const makeDiv = document.createElement('div');
-    // makeDiv.classList.add('menu_title_con')
+  //   // const makeDiv = document.createElement('div');
+  //   // makeDiv.classList.add('menu_title_con')
     
-  }
+  // }
   // menuContainerLi.appendchild(makeDiv);
   
-  console.log(menuContainerLi.firstElementChild.length)
+  console.log(menuContainerUl)
 
    
   
